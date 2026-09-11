91ec0d8ad7cc9529f4904318991622b20608597b
Tony George <tg764562@gmail.com>
2026-09-11 05:42:27 +0300

    chore: migrate jvmTarget to compilerOptions DSL
    Silences the AGP deprecation warning in all modules. No behavior change.

--- workout-randomforest/build.gradle.kts
+++ workout-randomforest/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
@@ -35,8 +37,10 @@ android {
         isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
 }
 
